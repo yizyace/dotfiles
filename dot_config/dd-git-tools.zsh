41d404dfd0b047d41ab53607de6b2e1a8a7e3c81
Ace Yizy <yizyace@gmail.com>
2026-01-25 16:37:14 -1000

    feat(gg): add --help command with alias documentation

--- dot_config/dd-git-tools.zsh
+++ dot_config/dd-git-tools.zsh
@@ -12,6 +12,37 @@ gg() {
 
   # Custom commands
   case "$cmd" in
+    --help|-h|help)
+      cat <<'HELP'
+gg - git wrapper with shortcuts
+
+ALIASES (expand to git commands):
+  co        checkout
+  cob       checkout -b (create and switch to new branch)
+  cm        commit
+  st        status
+  br        branch -a (list all branches)
+  dt        difftool
+  tack      commit -a --amend (amend with all changes)
+  lg        log with graph and pretty format
+  lgo       log --oneline
+
+CUSTOM COMMANDS:
+  recent [n]     Show last n branches visited (default: 10)
+  conflicts      List files with merge conflicts
+  g <pattern>    Grep with nice formatting
+  search <pat>   Case-insensitive grep
+  logsearch <p>  Search commit messages
+  ec             Edit local git config
+  egc            Edit global git config
+  ac <msg>       Stage all and commit with message
+  wipe           Create savepoint, then reset hard
+  qq             Interactive file picker for status
+
+All other commands are passed to git.
+HELP
+      return
+      ;;
     recent)
       local n="${1:-10}"
       git reflog show --pretty=format:'%gs' | grep 'checkout: moving' | sed 's/checkout: moving from .* to //' | awk '!seen[$0]++' | head -n "$n"
